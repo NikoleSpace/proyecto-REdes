80e58c3f6f556ce2ae1f900d662a95727e1e6995
Nikole Scarlet <129306932+NikoleSpace@users.noreply.github.com>
2024-06-25 10:08:56 -0500

    solo frontend

--- api-terramine-file-manager/api-terramine/src/models/areaModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const areaSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    employees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] // Lista de empleados que pertenecen a esta área
-});
-
-module.exports = mongoose.model('Area', areaSchema);
